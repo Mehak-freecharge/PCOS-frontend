9a5477373f8d943177d12ab0b7b125d793c75474
Himanshi Sharma <himanshish456@gmail.com>
2024-04-01 12:28:05 +0530

    hero section & responsive

--- pcos-detection/src/components/Hero.jsx
+++ pcos-detection/src/components/Hero.jsx
@@ -1,5 +1,4 @@
 import { motion } from "framer-motion";
-import logo from "../images/logo.png";
 
 const textVariants = {
   initial: {
@@ -26,28 +25,35 @@ const textVariants = {
 
 const Hero = () => {
   return (
-    <div className="mt-40 h-[calc(100vh - 100px)] overflow-hidden ">
-      <div className="max-w-[1366px] h-full m-auto">
+    <div className="overflow-hidden justify-center items-center flex flex-col">
+      <div className="max-w-[1366px] h-full m-auto flex flex-col md:flex-row-reverse gap-10">
+        <div className="h-full top-0 right-0 md:w-1/2 mt-10">
+          <img
+            src="./hero.png"
+            alt=""
+            className="w-full h-full object-contain"
+          />
+        </div>
         <motion.div
-          className="w-1/2 h-full flex flex-col justify-center gap-10"
+          className="w-full md:w-1/2 h-full flex flex-col justify-center gap-10 lg:mt-40 md:mt-20"
           variants={textVariants}
           initial="initial"
           animate="animate"
         >
-          <motion.h2 variants={textVariants} className="text-3xl">
-            Welcome to sheAura
+          <motion.h2 variants={textVariants} className="text-7xl text-gray-700">
+            Welcome to sheAura!!
           </motion.h2>
-          <motion.p variants={textVariants} className="text-2xl">
+          <motion.p variants={textVariants} className="text-xl text-gray-500">
             Polycystic Ovary Syndrome (PCOS) is a common hormonal disorder among
             women.
           </motion.p>
-          <motion.p variants={textVariants} className="text-2xl">
+          <motion.p variants={textVariants} className="text-xl text-gray-500">
             We have developed this app to help women with polycystic ovary
             syndrome (PCOS) find information of the highest quality from leading
             experts in this field. It has been designed with women and for women
             with polycystic ovary syndrome.
           </motion.p>
-          <motion.p variants={textVariants} className="text-2xl">
+          <motion.p variants={textVariants} className="text-xl text-gray-500">
             Having PCOS is a journey of learning about the condition, of
             understanding how it impacts your life and of what you can do to
             manage this. We are providing trustworthy information to learn about
@@ -55,14 +61,6 @@ const Hero = () => {
             lifestyle for you.
           </motion.p>
         </motion.div>
-      </div>
-      <div className="h-full absolute top-0 right-0 flex flex-col">
-        <img
-          src="./hero.png"
-          alt=""
-          className="mt-20 w-full h-full object-cover"
-        />
-        {/* <img src={logo} alt="" className="w-full h-full object-cover" /> */}
       </div>
     </div>
   );
