33de20edd9264ff4a2c20677314ce4171eb438f8
Himanshi Sharma <himanshish456@gmail.com>
2024-04-12 10:19:00 +0530

    files

--- pcos-detection/src/home/Home.jsx
+++ pcos-detection/src/home/Home.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Explore from "../components/Explore";
+import Search from "../components/Search";
+import Lifestyle from "../components/Lifestyle";
+import Forum from "../components/Forum";
+import Contact from "../components/Contact";
 
 const Home = () => {
   return (
@@ -8,8 +12,20 @@ const Home = () => {
       <div id="Home">
         <Hero />
       </div>
+      <div id="Search">
+        <Search />
+      </div>
       <div id="Explore">
         <Explore />
+      </div>
+      <div id="Lifestyle">
+        <Lifestyle />
+      </div>
+      <div id="Forum">
+        <Forum />
+      </div>
+      <div id="Contact">
+        <Contact />
       </div>
     </div>
   );
