a9fb0dd0156aeb45fc3fea2db6f284e0c2f9df70
Himanshi Sharma <himanshish456@gmail.com>
2024-04-18 13:14:37 +0530

    explore

--- pcos-detection/src/home/Home.jsx
+++ pcos-detection/src/home/Home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Hero from "../components/Hero";
 import Explore from "../components/Explore";
 import Search from "../components/Search";
@@ -6,18 +5,54 @@ import Lifestyle from "../components/Lifestyle";
 import Forum from "../components/Forum";
 import Contact from "../components/Contact";
 
+import React, { useEffect, useRef, useState } from "react";
+
+const RevealOnScroll = ({ children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const onWindScroll = () => {
+      const element = ref.current;
+      if (element) {
+        const { top } = element.getBoundingClientRect();
+        const isVisible = top < window.innerHeight;
+        setIsVisible(isVisible);
+      }
+    };
+
+    window.addEventListener("scroll", onWindScroll);
+    return () => {
+      window.removeEventListener("scroll", onWindScroll);
+    };
+  }, []);
+
+  const classes = `transition-opacity duration-1000
+        ${isVisible ? "opacity-100" : "opacity-0"}`;
+
+  return (
+    <div ref={ref} className={classes}>
+      {children}
+    </div>
+  );
+};
+
 const Home = () => {
   return (
     <div>
       <div id="Home">
         <Hero />
       </div>
-      <div id="Search">
-        <Search />
-      </div>
-      <div id="Explore">
-        <Explore />
-      </div>
+      {/* <RevealOnScroll>
+        <div id="Search">
+          <Search />
+        </div>
+      </RevealOnScroll> */}
+      <RevealOnScroll>
+        <div id="Explore">
+          <Explore />
+        </div>
+      </RevealOnScroll>
       <div id="Lifestyle">
         <Lifestyle />
       </div>
